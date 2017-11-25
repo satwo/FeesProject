36f665266083df18d4b059f13df57df6a1b7b8bc
Samuel B. Atwood <samuel.atwood@gmail.com>
2017-11-25 11:53:58 -0600

    Add Tx Fees (BTC) & Tx Size charts

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,10 +8,65 @@ fetch('./data.json').then(function (response) {
 }).then(function (data) {
   blockData = data.sort(function (a, b) { return a.Height - b.Height; });
 
-  var sliced = blockData.slice(blockData.length-12, blockData.length);
+  var sliced = blockData.slice(blockData.length - 12, blockData.length);
 
   RenderCharts(sliced);
 });
+
+var AxisLabelFont = {
+  size: 24
+}
+
+var xAxisLabel = {
+  value: "Block Height",
+  font: AxisLabelFont
+}
+
+var xAxis = {
+  value: "Height",
+  axis: true,
+  grid: true,
+  label: xAxisLabel
+}
+
+var yAxisLabelSpB = {
+  value: "Satoshis / Byte",
+  font: AxisLabelFont
+}
+
+var yAxisLabelTxFees = {
+  value: "BTC / Tx",
+  font: AxisLabelFont
+}
+
+var yAxisLabelTxSize = {
+  value: "Tx Size (Bytes)",
+  font: AxisLabelFont
+}
+
+var yAxisSpB = {
+  value: "SPB",
+  axis: true,
+  grid: true,
+  label: yAxisLabelSpB
+}
+
+var yAxisTxFees = {
+  value: "txFee",
+  axis: true,
+  grid: true,
+  label: yAxisLabelTxFees
+  // mute: isOutlier
+}
+
+var yAxisTxSize = {
+  value: "txSize",
+  axis: true,
+  grid: true,
+  label: yAxisLabelTxSize
+  // mute: isOutlier
+}
+
 
 var visualization;
 var legacyData = [];
@@ -21,60 +76,34 @@ function RenderCharts(blockData) {
   blockRange = blockData.map(a => a.Height);
 
   blockData.forEach(block => {
-    arrayOfLegacyFees = block.LegacyTxData.satsPerByte_list;
-    arrayOfLegacyFees.forEach(fee => {
-      dataPoint = {
-        "Height": block.Height,
-        "name": Math.random(),
-        "SPB": fee
-      };
-      legacyData.push(dataPoint);
-    });
 
-    arrayOfSegwitFees = block.SegWitTxData.satsPerByte_list;
-    arrayOfSegwitFees.forEach(fee => {
+    segwitTxCount = block.SegWitTxData.txCount;
+
+    for (var i = 0; i < segwitTxCount; i++) {
       dataPoint = {
         "Height": block.Height,
         "name": Math.random(),
-        "SPB": fee
+        "SPB": block.SegWitTxData.satsPerByte_list[i],
+        "txFee": block.SegWitTxData.txFees_list[i],
+        "txSize": block.SegWitTxData.txSize_list[i]
       };
       segwitData.push(dataPoint);
-    });
+    }
+
+    legacyTxCount = block.SegWitTxData.txCount;
+
+    for (var i = 0; i < legacyTxCount; i++) {
+      dataPoint = {
+        "Height": block.Height,
+        "name": Math.random(),
+        "SPB": block.LegacyTxData.satsPerByte_list[i],
+        "txFee": block.LegacyTxData.txFees_list[i],
+        "txSize": block.LegacyTxData.txSize_list[i]
+      };
+      legacyData.push(dataPoint);
+    }
+
   });
-
-  var AxisLabelFont = {
-    size: 24
-  }
-
-  var xAxisLabel = {
-    value: "Block Height", 
-    font: AxisLabelFont 
-  }
-
-  var xAxis = {
-    value: "Height",
-    axis: true,
-    grid: true,
-    label: xAxisLabel,
-   // ticks: xTicks,
-    padding: 100
-  }
-
-  var yAxisLabel = {
-    value: "Satoshis/Byte", 
-    font: AxisLabelFont
-  }
-
-  var yAxis = {
-    value: "SPB",
-    axis: true,
-    grid: true,
-    label: yAxisLabel,
-  //  ticks: yTicks,
-    padding: 100,
-    // mute: isOutlier
-  }
-
   //var outlierRange = outlierRange(data.map(d=>d.value))
   //var isOutlier = val =>  (val < outlierRange[0] || val > outlierRange[1])
 
@@ -84,7 +113,7 @@ function RenderCharts(blockData) {
     .type("box")
     .id("name")
     .x(xAxis)
-    .y(yAxis)
+    .y(yAxisSpB)
     //.background("#a9a9a9")
     .format({
       "text": function (text, params) {
@@ -108,15 +137,44 @@ function RenderCharts(blockData) {
       }
     })
     .draw()
+
+  console.log("DID NOT GET HERE");
+
 }
 
-function showSegwitData(){
-    visualization.data(segwitData).draw();
+function showSegwitData() {
+  console.log("HIT");
+  console.log(segwitData);
+  visualization
+    .data(segwitData)
+    .draw();
 }
 
-function showLegacyData(){
-  visualization.data(legacyData).draw();
+function showLegacyData() {
+  visualization
+    .data(legacyData)
+    .draw();
 }
+
+function showTxFees() {
+  visualization
+    .y(yAxisTxFees)
+    .draw();
+}
+
+function showTxSize() {
+  visualization
+    .y(yAxisTxSize)
+    .draw();
+}
+
+function showFeesSpB() {
+  visualization
+    .y(yAxisSpB)
+    .draw();
+}
+
+
 
 
 
