2c3d1103d3ec0ba4a1ab21fb6e05205b435ef138
Samuel B. Atwood <samuel.atwood@gmail.com>
2017-11-26 11:19:40 -0600

    Add option to toggle visibility of extreme outliers in data

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,13 +2,22 @@
 
 var blockRange;
 var blockData;
+var showOutliers = true;
+var txType = "SegWit";
+var chartType = "SPB";
+
+var yAxisSpB;
+var yAxisTxFees;
+var yAxisTxSize;
+var range;
+var isOutlier;
 
 fetch('./data.json').then(function (response) {
   return response.json();
 }).then(function (data) {
   blockData = data.sort(function (a, b) { return a.Height - b.Height; });
 
-  var sliced = blockData.slice(blockData.length - 12, blockData.length);
+  var sliced = blockData.slice(blockData.length - 36, blockData.length);
 
   RenderCharts(sliced);
 });
@@ -44,27 +53,32 @@ var yAxisLabelTxSize = {
   font: AxisLabelFont
 }
 
-var yAxisSpB = {
-  value: "SPB",
-  axis: true,
-  grid: true,
-  label: yAxisLabelSpB
-}
 
-var yAxisTxFees = {
-  value: "txFee",
-  axis: true,
-  grid: true,
-  label: yAxisLabelTxFees
-  // mute: isOutlier
-}
 
-var yAxisTxSize = {
-  value: "txSize",
-  axis: true,
-  grid: true,
-  label: yAxisLabelTxSize
-  // mute: isOutlier
+
+
+function outlierRange(someArray) {
+
+  if (someArray.length < 4)
+    return someArray;
+
+  let values, q1, q3, iqr, maxValue, minValue, multiple = 3.5;
+
+  values = someArray.slice().sort((a, b) => a - b);//copy array fast and sort
+
+  if ((values.length / 4) % 1 === 0) {//find quartiles
+    q1 = 1 / 2 * (values[(values.length / 4)] + values[(values.length / 4) + 1]);
+    q3 = 1 / 2 * (values[(values.length * (3 / 4))] + values[(values.length * (3 / 4)) + 1]);
+  } else {
+    q1 = values[Math.floor(values.length / 4 + 1)];
+    q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
+  }
+
+  iqr = q3 - q1;
+  maxValue = q3 + iqr * multiple;
+  minValue = q1 - iqr * multiple;
+
+  return [minValue, maxValue]
 }
 
 
@@ -104,8 +118,32 @@ function RenderCharts(blockData) {
     }
 
   });
-  //var outlierRange = outlierRange(data.map(d=>d.value))
-  //var isOutlier = val =>  (val < outlierRange[0] || val > outlierRange[1])
+
+
+  yAxisTxFees = {
+    value: "txFee",
+    axis: true,
+    grid: true,
+    label: yAxisLabelTxFees,
+    mute: isOutlier
+  }
+  
+  yAxisTxSize = {
+    value: "txSize",
+    axis: true,
+    grid: true,
+    label: yAxisLabelTxSize,
+    mute: isOutlier
+  }
+
+
+  yAxisSpB = {
+    value: "SPB",
+    axis: true,
+    grid: true,
+    label: yAxisLabelSpB,
+    mute: isOutlier
+  }
 
   visualization = d3plus.viz()
     .container("#txFees")
@@ -138,40 +176,136 @@ function RenderCharts(blockData) {
     })
     .draw()
 
-  console.log("DID NOT GET HERE");
+}
 
+function setRange(txType, chartType) {
+
+  console.log("Setting range...");
+  console.log("tx Type: " + txType);
+  console.log("Chart Type: " + chartType);
+  console.log("Show Outliers? " + showOutliers);
+
+  var data;
+
+  switch (txType) {
+    case "Legacy":
+      data = legacyData;
+      break;
+    case "SegWit":
+      data = segwitData;
+      break;
+    default:
+      break;
+  }
+
+  switch (chartType) {
+    case "SPB":
+      range = outlierRange(data.map(d => d.SPB))
+      break;
+    case "txFee":
+      range = outlierRange(data.map(d => d.txFee))
+      break;
+    case "txSize":
+      range = outlierRange(data.map(d => d.txSize))
+      break;
+    default:
+      break;
+
+  }
+
+  isOutlier = val => (val > range[1]);
+
+  yAxisSpB["mute"] = showOutliers ? [] : isOutlier;
+  yAxisTxFees["mute"] = showOutliers ? [] : isOutlier;
+  yAxisTxSize["mute"] = showOutliers ? [] : isOutlier;
 }
 
 function showSegwitData() {
-  console.log("HIT");
-  console.log(segwitData);
+
+  txType = "SegWit";
+
+  setRange(txType, chartType);
+
   visualization
     .data(segwitData)
     .draw();
 }
 
 function showLegacyData() {
+
+  txType = "Legacy";
+
+  setRange(txType, chartType);
+
   visualization
     .data(legacyData)
     .draw();
 }
 
 function showTxFees() {
+
+  chartType = "txFee";
+
+  setRange(txType, chartType);
+
   visualization
     .y(yAxisTxFees)
     .draw();
 }
 
 function showTxSize() {
+
+  chartType = "txSize";
+
+  setRange(txType, chartType);
+
   visualization
     .y(yAxisTxSize)
     .draw();
 }
 
 function showFeesSpB() {
+
+  chartType = "SPB";
+
+  setRange(txType, chartType);
+
   visualization
     .y(yAxisSpB)
     .draw();
+}
+
+function toggleOutliers(cb) {
+  showOutliers = cb.checked;
+
+  console.log("Toggling Outliers..");
+  console.log("tx Type: " + txType);
+  console.log("Chart Type: " + chartType);
+  console.log("Show Outliers? " + showOutliers);
+
+
+  yAxisSpB["mute"] = showOutliers ? [] : isOutlier;
+  yAxisTxFees["mute"] = showOutliers ? [] : isOutlier;
+  yAxisTxSize["mute"] = showOutliers ? [] : isOutlier;
+
+  //console.log(yAxisSpB);
+  //console.log(yAxisTxFees);
+  //console.log(yAxisTxSize);
+
+  switch (chartType) {
+    case "SPB":
+      visualization.y(yAxisSpB).draw();
+      break;
+    case "txFee":
+      visualization.y(yAxisTxFees).draw();
+      break;
+    case "txSize":
+      visualization.y(yAxisTxSize).draw();
+      break;
+    default:
+      visualization.draw();
+  }
+
 }
 
 
