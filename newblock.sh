4e0f14075a6553c7145d54b079aea7a64eea41d7
Samuel B. Atwood <samuel.atwood@gmail.com>
2017-11-26 21:09:25 -0600

    place lock on script to prevent multiple instances from running concurrenctly

--- newblock.sh
+++ newblock.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python "getfeehistory.py"
+(
+  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
+  flock -x -w 10 200 || exit 1
+
+  python "getfeehistory.py"
+
+) 200>/var/lock/.myscript.exclusivelock
