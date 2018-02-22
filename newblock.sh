a77eb592144297e426bc82c5ae71fc19aae346ac
Samuel B. Atwood <samuel.atwood@gmail.com>
2018-02-22 03:50:23 -0600

    Use python2 for bitcoin.rpc

--- newblock.sh
+++ newblock.sh
@@ -3,6 +3,6 @@
   # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
   flock -x -w 10 200 || exit 1
 
-  python "getfeehistory.py"
+  python2 "getfeehistory.py"
 
 ) 200>/var/lock/.myscript.exclusivelock
